d9ed0261c56c3558a9032793094c27c1f030b07f
Aaron Powell <me@aaron-powell.com>
2022-06-20 04:04:46 +0000

    formatting on generation

--- backend/src/generated/graphql.ts
+++ backend/src/generated/graphql.ts
@@ -1,10 +1,18 @@
-import { GraphQLResolveInfo } from 'graphql';
+import { GraphQLResolveInfo } from "graphql";
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
-export type RequireFields<T, K extends keyof T> = Omit<T, K> & { [P in K]-?: NonNullable<T[P]> };
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
+export type RequireFields<T, K extends keyof T> = Omit<T, K> & {
+  [P in K]-?: NonNullable<T[P]>;
+};
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -15,70 +23,68 @@ export type Scalars = {
 };
 
 export type Game = Node & {
-  __typename?: 'Game';
+  __typename?: "Game";
   answers: Array<PlayerAnswer>;
-  id: Scalars['ID'];
+  id: Scalars["ID"];
   players: Array<Player>;
   questions: Array<Question>;
   state: GameState;
 };
 
 export enum GameState {
-  Completed = 'Completed',
-  Started = 'Started',
-  WaitingForPlayers = 'WaitingForPlayers'
+  Completed = "Completed",
+  Started = "Started",
+  WaitingForPlayers = "WaitingForPlayers",
 }
 
 export type Node = {
-  id: Scalars['ID'];
+  id: Scalars["ID"];
 };
 
 export type Player = Node & {
-  __typename?: 'Player';
+  __typename?: "Player";
   answers: Array<PlayerAnswer>;
   games: Array<Game>;
-  id: Scalars['ID'];
-  name: Scalars['String'];
+  id: Scalars["ID"];
+  name: Scalars["String"];
 };
 
 export type PlayerAnswer = Node & {
-  __typename?: 'PlayerAnswer';
-  answer: Scalars['String'];
+  __typename?: "PlayerAnswer";
+  answer: Scalars["String"];
   game: Game;
-  id: Scalars['ID'];
+  id: Scalars["ID"];
   player: Player;
   question?: Maybe<Question>;
 };
 
 export type Query = {
-  __typename?: 'Query';
+  __typename?: "Query";
   game?: Maybe<Game>;
   games: Array<Game>;
 };
 
-
 export type QueryGameArgs = {
-  id: Scalars['ID'];
+  id: Scalars["ID"];
 };
 
 export type Question = Node & {
-  __typename?: 'Question';
-  answers: Array<Scalars['String']>;
-  correctAnswer: Scalars['String'];
+  __typename?: "Question";
+  answers: Array<Scalars["String"]>;
+  correctAnswer: Scalars["String"];
   games: Array<Game>;
-  id: Scalars['ID'];
-  question: Scalars['String'];
+  id: Scalars["ID"];
+  question: Scalars["String"];
 };
 
-
-
 export type ResolverTypeWrapper<T> = Promise<T> | T;
-
 
 export type ResolverWithResolve<TResult, TParent, TContext, TArgs> = {
   resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
 };
-export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> = ResolverFn<TResult, TParent, TContext, TArgs> | ResolverWithResolve<TResult, TParent, TContext, TArgs>;
+export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> =
+  | ResolverFn<TResult, TParent, TContext, TArgs>
+  | ResolverWithResolve<TResult, TParent, TContext, TArgs>;
 
 export type ResolverFn<TResult, TParent, TContext, TArgs> = (
   parent: TParent,
@@ -101,9 +107,25 @@ export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
   info: GraphQLResolveInfo
 ) => TResult | Promise<TResult>;
 
-export interface SubscriptionSubscriberObject<TResult, TKey extends string, TParent, TContext, TArgs> {
-  subscribe: SubscriptionSubscribeFn<{ [key in TKey]: TResult }, TParent, TContext, TArgs>;
-  resolve?: SubscriptionResolveFn<TResult, { [key in TKey]: TResult }, TContext, TArgs>;
+export interface SubscriptionSubscriberObject<
+  TResult,
+  TKey extends string,
+  TParent,
+  TContext,
+  TArgs
+> {
+  subscribe: SubscriptionSubscribeFn<
+    { [key in TKey]: TResult },
+    TParent,
+    TContext,
+    TArgs
+  >;
+  resolve?: SubscriptionResolveFn<
+    TResult,
+    { [key in TKey]: TResult },
+    TContext,
+    TArgs
+  >;
 }
 
 export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
@@ -111,12 +133,26 @@ export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
   resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
 }
 
-export type SubscriptionObject<TResult, TKey extends string, TParent, TContext, TArgs> =
+export type SubscriptionObject<
+  TResult,
+  TKey extends string,
+  TParent,
+  TContext,
+  TArgs
+> =
   | SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
   | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
 
-export type SubscriptionResolver<TResult, TKey extends string, TParent = {}, TContext = {}, TArgs = {}> =
-  | ((...args: any[]) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
+export type SubscriptionResolver<
+  TResult,
+  TKey extends string,
+  TParent = {},
+  TContext = {},
+  TArgs = {}
+> =
+  | ((
+      ...args: any[]
+    ) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
   | SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
 
 export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
@@ -125,11 +161,20 @@ export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
   info: GraphQLResolveInfo
 ) => Maybe<TTypes> | Promise<Maybe<TTypes>>;
 
-export type IsTypeOfResolverFn<T = {}, TContext = {}> = (obj: T, context: TContext, info: GraphQLResolveInfo) => boolean | Promise<boolean>;
+export type IsTypeOfResolverFn<T = {}, TContext = {}> = (
+  obj: T,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => boolean | Promise<boolean>;
 
 export type NextResolverFn<T> = () => Promise<T>;
 
-export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs = {}> = (
+export type DirectiveResolverFn<
+  TResult = {},
+  TParent = {},
+  TContext = {},
+  TArgs = {}
+> = (
   next: NextResolverFn<TResult>,
   parent: TParent,
   args: TArgs,
@@ -139,73 +184,124 @@ export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs
 
 /** Mapping between all available schema types and the resolvers types */
 export type ResolversTypes = {
-  Boolean: ResolverTypeWrapper<Scalars['Boolean']>;
+  Boolean: ResolverTypeWrapper<Scalars["Boolean"]>;
   Game: ResolverTypeWrapper<Game>;
   GameState: GameState;
-  ID: ResolverTypeWrapper<Scalars['ID']>;
-  Node: ResolversTypes['Game'] | ResolversTypes['Player'] | ResolversTypes['PlayerAnswer'] | ResolversTypes['Question'];
+  ID: ResolverTypeWrapper<Scalars["ID"]>;
+  Node:
+    | ResolversTypes["Game"]
+    | ResolversTypes["Player"]
+    | ResolversTypes["PlayerAnswer"]
+    | ResolversTypes["Question"];
   Player: ResolverTypeWrapper<Player>;
   PlayerAnswer: ResolverTypeWrapper<PlayerAnswer>;
   Query: ResolverTypeWrapper<{}>;
   Question: ResolverTypeWrapper<Question>;
-  String: ResolverTypeWrapper<Scalars['String']>;
+  String: ResolverTypeWrapper<Scalars["String"]>;
 };
 
 /** Mapping between all available schema types and the resolvers parents */
 export type ResolversParentTypes = {
-  Boolean: Scalars['Boolean'];
+  Boolean: Scalars["Boolean"];
   Game: Game;
-  ID: Scalars['ID'];
-  Node: ResolversParentTypes['Game'] | ResolversParentTypes['Player'] | ResolversParentTypes['PlayerAnswer'] | ResolversParentTypes['Question'];
+  ID: Scalars["ID"];
+  Node:
+    | ResolversParentTypes["Game"]
+    | ResolversParentTypes["Player"]
+    | ResolversParentTypes["PlayerAnswer"]
+    | ResolversParentTypes["Question"];
   Player: Player;
   PlayerAnswer: PlayerAnswer;
   Query: {};
   Question: Question;
-  String: Scalars['String'];
+  String: Scalars["String"];
 };
 
-export type GameResolvers<ContextType = any, ParentType extends ResolversParentTypes['Game'] = ResolversParentTypes['Game']> = {
-  answers?: Resolver<Array<ResolversTypes['PlayerAnswer']>, ParentType, ContextType>;
-  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
-  players?: Resolver<Array<ResolversTypes['Player']>, ParentType, ContextType>;
-  questions?: Resolver<Array<ResolversTypes['Question']>, ParentType, ContextType>;
-  state?: Resolver<ResolversTypes['GameState'], ParentType, ContextType>;
+export type GameResolvers<
+  ContextType = any,
+  ParentType extends ResolversParentTypes["Game"] = ResolversParentTypes["Game"]
+> = {
+  answers?: Resolver<
+    Array<ResolversTypes["PlayerAnswer"]>,
+    ParentType,
+    ContextType
+  >;
+  id?: Resolver<ResolversTypes["ID"], ParentType, ContextType>;
+  players?: Resolver<Array<ResolversTypes["Player"]>, ParentType, ContextType>;
+  questions?: Resolver<
+    Array<ResolversTypes["Question"]>,
+    ParentType,
+    ContextType
+  >;
+  state?: Resolver<ResolversTypes["GameState"], ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 };
 
-export type NodeResolvers<ContextType = any, ParentType extends ResolversParentTypes['Node'] = ResolversParentTypes['Node']> = {
-  __resolveType: TypeResolveFn<'Game' | 'Player' | 'PlayerAnswer' | 'Question', ParentType, ContextType>;
-  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+export type NodeResolvers<
+  ContextType = any,
+  ParentType extends ResolversParentTypes["Node"] = ResolversParentTypes["Node"]
+> = {
+  __resolveType: TypeResolveFn<
+    "Game" | "Player" | "PlayerAnswer" | "Question",
+    ParentType,
+    ContextType
+  >;
+  id?: Resolver<ResolversTypes["ID"], ParentType, ContextType>;
 };
 
-export type PlayerResolvers<ContextType = any, ParentType extends ResolversParentTypes['Player'] = ResolversParentTypes['Player']> = {
-  answers?: Resolver<Array<ResolversTypes['PlayerAnswer']>, ParentType, ContextType>;
-  games?: Resolver<Array<ResolversTypes['Game']>, ParentType, ContextType>;
-  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
-  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+export type PlayerResolvers<
+  ContextType = any,
+  ParentType extends ResolversParentTypes["Player"] = ResolversParentTypes["Player"]
+> = {
+  answers?: Resolver<
+    Array<ResolversTypes["PlayerAnswer"]>,
+    ParentType,
+    ContextType
+  >;
+  games?: Resolver<Array<ResolversTypes["Game"]>, ParentType, ContextType>;
+  id?: Resolver<ResolversTypes["ID"], ParentType, ContextType>;
+  name?: Resolver<ResolversTypes["String"], ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 };
 
-export type PlayerAnswerResolvers<ContextType = any, ParentType extends ResolversParentTypes['PlayerAnswer'] = ResolversParentTypes['PlayerAnswer']> = {
-  answer?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  game?: Resolver<ResolversTypes['Game'], ParentType, ContextType>;
-  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
-  player?: Resolver<ResolversTypes['Player'], ParentType, ContextType>;
-  question?: Resolver<Maybe<ResolversTypes['Question']>, ParentType, ContextType>;
+export type PlayerAnswerResolvers<
+  ContextType = any,
+  ParentType extends ResolversParentTypes["PlayerAnswer"] = ResolversParentTypes["PlayerAnswer"]
+> = {
+  answer?: Resolver<ResolversTypes["String"], ParentType, ContextType>;
+  game?: Resolver<ResolversTypes["Game"], ParentType, ContextType>;
+  id?: Resolver<ResolversTypes["ID"], ParentType, ContextType>;
+  player?: Resolver<ResolversTypes["Player"], ParentType, ContextType>;
+  question?: Resolver<
+    Maybe<ResolversTypes["Question"]>,
+    ParentType,
+    ContextType
+  >;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 };
 
-export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = {
-  game?: Resolver<Maybe<ResolversTypes['Game']>, ParentType, ContextType, RequireFields<QueryGameArgs, 'id'>>;
-  games?: Resolver<Array<ResolversTypes['Game']>, ParentType, ContextType>;
+export type QueryResolvers<
+  ContextType = any,
+  ParentType extends ResolversParentTypes["Query"] = ResolversParentTypes["Query"]
+> = {
+  game?: Resolver<
+    Maybe<ResolversTypes["Game"]>,
+    ParentType,
+    ContextType,
+    RequireFields<QueryGameArgs, "id">
+  >;
+  games?: Resolver<Array<ResolversTypes["Game"]>, ParentType, ContextType>;
 };
 
-export type QuestionResolvers<ContextType = any, ParentType extends ResolversParentTypes['Question'] = ResolversParentTypes['Question']> = {
-  answers?: Resolver<Array<ResolversTypes['String']>, ParentType, ContextType>;
-  correctAnswer?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
-  games?: Resolver<Array<ResolversTypes['Game']>, ParentType, ContextType>;
-  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
-  question?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+export type QuestionResolvers<
+  ContextType = any,
+  ParentType extends ResolversParentTypes["Question"] = ResolversParentTypes["Question"]
+> = {
+  answers?: Resolver<Array<ResolversTypes["String"]>, ParentType, ContextType>;
+  correctAnswer?: Resolver<ResolversTypes["String"], ParentType, ContextType>;
+  games?: Resolver<Array<ResolversTypes["Game"]>, ParentType, ContextType>;
+  id?: Resolver<ResolversTypes["ID"], ParentType, ContextType>;
+  question?: Resolver<ResolversTypes["String"], ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 };
 
@@ -217,4 +313,3 @@ export type Resolvers<ContextType = any> = {
   Query?: QueryResolvers<ContextType>;
   Question?: QuestionResolvers<ContextType>;
 };
-
